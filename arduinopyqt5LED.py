baa3ace66714fd0289c9bf755dbc7620b3eaa75d
HelloLee7 <jhelee920@gmail.com>
2024-11-13 12:17:39 +0900

    last5

--- arduinopyqt5LED.py
+++ arduinopyqt5LED.py
@@ -5,12 +5,15 @@ import time
 ser = serial.Serial('COM4', 9600)  # Windows의 경우 COM 포트 사용
 time.sleep(0.5)  # 아두이노 초기화 시간 대기
 
-# LED 켜기
-ser.write(b'1')
-time.sleep(0.5)  # 2초 대기
+# 플래그 변수 설정
+led_on = True
 
-# LED 끄기
-ser.write(b'0')
-time.sleep(0.5)  # 2초 대기
+# 플래그에 따라 LED 제어
+if led_on:
+    ser.write(b'1')  # LED 켜기
+else:
+    ser.write(b'0')  # LED 끄기
+
+time.sleep(0.5)  # 대기 시간
 
 ser.close()
